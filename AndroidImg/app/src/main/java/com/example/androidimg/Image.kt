94e97f764cf9f3c708dbfcff1474e6d60e85ff19
Cruzzardo <can71695@udelasalle.edu.mx>
2021-09-13 17:49:52 -0500

    Entidad
    Modificamos la clase imagen para asignar los valores a cada objeto

--- AndroidImg/app/src/main/java/com/example/androidimg/Image.kt
+++ AndroidImg/app/src/main/java/com/example/androidimg/Image.kt
@@ -13,17 +13,19 @@ class Image(
     var title: String = "",
     var information: String = "",
     var source: Int = 0,
-    var star: Boolean? = null
+    var star: Boolean? = null,
+    var favorite: Boolean? = null,
+    var sound: Int = 0
 
 ) : Parcelable {
     companion object {
         val images = arrayOf(
-            Image(1, "Castillo", "Castillo que lo puse porque no sabia que poner, pero estaba chido", R.drawable.ic_castillo_de_bran, false),
-            Image(2, "Instagram", "Es una aplicación y red social de origen estadounidense, propiedad de Facebook, cuya función principal es poder compartir fotografías y vídeos con otros usuarios.", R.drawable.instagram, false),
-            Image(3, "TikTok", "Tik Tok es una red social de origen chino que consiste en crear videos cortos para compartirlos", R.drawable.tik_tok, false),
-            Image(4, "Sol", "El Sol es una estrella, es decir, un cuerpo celeste que brilla con luz propia, compuesto de hidrógeno y helio a enormes temperaturas en estado de plasma.", R.drawable.sol, false),
-            Image(5, "Rayo", "Es egocéntrico, arrogante, presumido y confiado, creyendo que puede ganar la Copa Piston por su cuenta", R.drawable.rayo, false),
-            Image(6, "Caballero", "Un jinete o persona que monta a caballo o, más estrictamente, una persona de origen noble", R.drawable.caballero, false)
+            Image(1, "Castillo", "Castillo que lo puse porque no sabia que poner, pero estaba chido", R.drawable.ic_castillo_de_bran, false,false, R.raw.castillo),
+            Image(2, "Instagram", "Es una aplicación y red social de origen estadounidense, propiedad de Facebook, cuya función principal es poder compartir fotografías y vídeos con otros usuarios.", R.drawable.instagram, false, false, R.raw.instagram),
+            Image(3, "TikTok", "Tik Tok es una red social de origen chino que consiste en crear videos cortos para compartirlos", R.drawable.tik_tok, false, false, R.raw.tiktok),
+            Image(4, "Sol", "El Sol es una estrella, es decir, un cuerpo celeste que brilla con luz propia, compuesto de hidrógeno y helio a enormes temperaturas en estado de plasma.", R.drawable.sol, false, false, R.raw.sol),
+            Image(5, "Rayo", "Es egocéntrico, arrogante, presumido y confiado, creyendo que puede ganar la Copa Piston por su cuenta", R.drawable.rayo, false, false, R.raw.rayo),
+            Image(6, "Caballero", "Un jinete o persona que monta a caballo o, más estrictamente, una persona de origen noble", R.drawable.caballero, false, false, R.raw.caballero)
         )
     }
 }
